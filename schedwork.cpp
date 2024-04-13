75161331e804d99a8f5726b5f1f60d66d894ef36
JasonGan <ganjason@usc.edu>
2024-04-12 21:30:53 -0700

    First Not Done

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,42 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool recursion(size_t day, size_t worker, const AvailabilityMatrix& avail, const size_t dailyNeed, vector<int>& shiftCount, DailySchedule& sched, vector<bool>& workerAssignedToday) {
+    if (day >= sched.size()) return true;
+
+    if (worker == 0) {
+        fill(workerAssignedToday.begin(), workerAssignedToday.end(), false); // Reset for new day
+    }
+
+    for (size_t w = 0; w < avail[day].size(); w++) 
+    {
+        if (avail[day][w] && shiftCount[w] > 0 && !workerAssignedToday[w]) 
+        {
+            sched[day][worker] = w;
+            shiftCount[w]--;
+            workerAssignedToday[w] = true;
+
+
+            if (worker + 1 < dailyNeed) 
+            {
+                if (recursion(day, worker + 1, avail, dailyNeed, shiftCount, sched, workerAssignedToday)) return true;
+            } 
+            else 
+            {
+                vector<bool> newWorkerAssignedToday(avail[day].size(), false);
+                if (recursion(day + 1, 0, avail, dailyNeed, shiftCount, sched, newWorkerAssignedToday)) return true;
+            }
+
+            // Backtrack
+            shiftCount[w]++;
+            workerAssignedToday[w] = false;
+            sched[day][worker] = INVALID_ID;
+        }
+    }
+
+    return false; // No valid assignment found
+}
+
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +73,10 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    vector<int> shiftCount(avail[0].size(), maxShifts);
+    vector<bool> workerAssignedToday(avail[0].size(), false);
+    size_t numDays = avail.size();
+    sched.resize(numDays, vector<Worker_T>(dailyNeed, INVALID_ID));
+    return recursion(0, 0, avail, dailyNeed, shiftCount, sched, workerAssignedToday);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,45 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void worldeRecursion(const std::string& in, const std::string& floatingRef, int* floating, std::set<std::string>& ans, const std::set<std::string>& dict, std::string& current, int n)
+{
+    if (n == in.size())
+    {
+        if (dict.find(current) != dict.end())
+        {
+            for (auto c: floatingRef)
+            {
+                if (floating[c - 'a'] > 0)
+                    return;
+            }
+            ans.insert(current);
+        }
+        return;
+    }
 
+    if (in[n] != '-')
+    {
+        worldeRecursion(in, floatingRef, floating, ans, dict, current, n+1);
+    }
+    else
+    {
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            if (floatingRef.find(c) != std::string::npos)
+            {
+                floating[c - 'a']--;
+                current[n] = c;
+                worldeRecursion(in, floatingRef, floating, ans, dict, current, n + 1);
+                floating[c - 'a']++;
+            }
+            else if (floatingRef.find(c) == std::string::npos)
+            {
+                current[n] = c;
+                worldeRecursion(in, floatingRef, floating, ans, dict, current, n + 1);
+            }
+        }
+    }
+}
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -21,8 +59,16 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
-
+  std::set<std::string> ans;
+  std::string current = in;
+  int free[26] = {};
+  for (auto ch : floating)
+  {
+    free[ch - 'a']++; 
+  }
+  
+  worldeRecursion(in, floating, free, ans, dict, current, 0);
+  return ans;
 }
 
 // Define any helper functions here
