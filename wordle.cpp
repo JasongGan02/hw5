1d12f72db81cf3a479f7815e7b86857b2c4abf11
JasonGan <ganjason@usc.edu>
2024-04-15 21:49:52 -0700

    done

--- wordle.cpp
+++ wordle.cpp
@@ -12,46 +12,9 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
-void worldeRecursion(const std::string& in, const std::string& floatingRef, int* floating, std::set<std::string>& ans, const std::set<std::string>& dict, std::string& current, int n)
-{
-    if (n == in.size())
-    {
-        if (dict.find(current) != dict.end())
-        {
-            for (auto c: floatingRef)
-            {
-                if (floating[c - 'a'] > 0)
-                    return;
-            }
-            ans.insert(current);
-        }
-        return;
-    }
 
-    if (in[n] != '-')
-    {
-        worldeRecursion(in, floatingRef, floating, ans, dict, current, n+1);
-    }
-    else
-    {
-        for (char c = 'a'; c <= 'z'; c++)
-        {
-            if (floatingRef.find(c) != std::string::npos)
-            {
-                floating[c - 'a']--;
-                current[n] = c;
-                worldeRecursion(in, floatingRef, floating, ans, dict, current, n + 1);
-                floating[c - 'a']++;
-            }
-            else if (floatingRef.find(c) == std::string::npos)
-            {
-                current[n] = c;
-                worldeRecursion(in, floatingRef, floating, ans, dict, current, n + 1);
-            }
-        }
-    }
-}
+// Add prototypes of helper functions here
+void wordleRecursion(const string& in, string floating, const set<string>& dict, size_t emp, string curr, set<string>& ans);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -60,15 +23,52 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
   std::set<std::string> ans;
-  std::string current = in;
-  int free[26] = {};
-  for (auto ch : floating)
+
+  size_t empty = 0;
+  for (auto c : in)
   {
-    free[ch - 'a']++; 
+    if (c == '-')
+        empty++;
   }
-  
-  worldeRecursion(in, floating, free, ans, dict, current, 0);
+  wordleRecursion(in, floating, dict, empty, "", ans);
   return ans;
+}
+
+void wordleRecursion(const string& in, string floating, const set<string>& dict, size_t emp, string curr, set<string>& ans)
+{
+    if (emp < floating.size())
+        return;
+    else if (curr.size() == in.size())
+    {
+        if (dict.find(curr) != dict.end() && floating.empty())
+        {
+            ans.insert(curr);
+        }
+    }
+    else if (in[curr.size()] != '-')
+    {
+        wordleRecursion(in, floating, dict, emp, curr + in[curr.size()], ans);
+    }
+    else
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            char c = char(97 + i);
+            size_t loc = floating.find(c);
+            if (loc != string::npos)
+            {
+                floating.erase(loc, 1);
+                wordleRecursion(in, floating, dict, emp - 1, curr + c, ans);
+                floating += c;
+            }
+            else
+            {
+                wordleRecursion(in, floating, dict, emp - 1, curr + c, ans);
+            }
+        }
+    }
+
+
 }
 
 // Define any helper functions here
